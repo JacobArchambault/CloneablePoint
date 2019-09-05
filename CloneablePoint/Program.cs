73ed253e1bdb7e6d04942aba4b3c209604a10bd1
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-05 12:20:16 -0400

    Make Point class implement ICloneable interface.

--- CloneablePoint/Program.cs
+++ CloneablePoint/Program.cs
@@ -10,6 +10,26 @@ namespace CloneablePoint
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Cloning *****\n");
+
+            // Construct points.
+            Point p1 = new Point(50, 50);
+            Point p2 = p1;
+            Point p3 = new Point(100, 100);
+            Point p4 = (Point)p3.Clone();
+
+            // Change p2(and p1)'s X coordinate to 0.
+            p2.X = 0;
+
+            // Change p4.X (this will not change p3.X).
+            p4.X = 0;
+
+            // Print each object.
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+            Console.ReadLine();
         }
     }
 }
